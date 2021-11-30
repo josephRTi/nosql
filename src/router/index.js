040206be067bb9ac446dd61d722d1db259219481
JosephRTi <josephrtix@gmail.com>
2021-11-30 22:39:22 +0300

    added base

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,43 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import TheIndex from "@/components/TheIndex";
+import TheHistory from "@/components/TheHistory";
+import Comparing from "@/components/Comparing";
+import TheUsage from "@/components/TheUsage";
+import TheTypes from "@/components/TheTypes";
 
 const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: TheIndex,
+    alias: '/index'
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/history',
+    name: 'History',
+    component: TheHistory
+  },
+  {
+    path: '/compare',
+    name: 'Сравнение',
+    component: Comparing
+  },
+  {
+    path: '/usage',
+    name: 'Использование',
+    component: TheUsage
+  },
+  {
+    path: '/types',
+    name: 'Типы NoSQL',
+    component: TheTypes
   }
 ]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
+  routes,
+  linkActiveClass: 'active'
 })
 
 export default router
